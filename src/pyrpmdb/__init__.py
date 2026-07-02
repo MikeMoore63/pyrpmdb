d919a7a720a44465d5ace16c696ef1769acde185
Mike Moore <z_z_zebra@yahoo.com>
2026-07-02 13:44:35 +0100

    feat: remove pyinstaller hooks no longefr required

--- src/pyrpmdb/__init__.py
+++ src/pyrpmdb/__init__.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import
 
-from pyrpmdb._pyinstaller import get_hook_dirs, get_PyInstaller_tests
 from pygorpmrustinfo import get_rpm_db_info
 
 __all__ = [
-    "get_hook_dirs",
-    "get_PyInstaller_tests",
     "get_rpm_db_info",
 ]
 
